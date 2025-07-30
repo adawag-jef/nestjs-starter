ca825bfef308561eaa9c2ae0d02e20b4fb1daf8e
jef-adawag <jeff.adawag@netroadshow.com>
2025-07-30 11:44:03 +0800

    add prisma and connect app to database

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@nestjs/common';
 // import { ConfigService } from '@nestjs/config';
 
 import { LoggerService } from './core/logger/logger.service';
+import { DatabaseService } from './database/database.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly logger: LoggerService) {}
+  constructor(
+    private readonly logger: LoggerService,
+    private readonly databaseService: DatabaseService,
+  ) {}
 
-  getHello(): string {
+  async getHello() {
     this.logger.log('calling log', 'App Service', { user: 123 });
+    const users = await this.databaseService.user.findMany();
+    console.log(users);
     return 'Hello World!';
   }
 }
